f2cda54632c114faada72bd470e59d56a599406b
Romanin <60302782+romanin-rf@users.noreply.github.com>
2025-01-09 05:43:17 +0200

    Update 0.6.2

--- seaplayer_audio/streamers/mpstreamersnd/_types.py
+++ seaplayer_audio/streamers/mpstreamersnd/_types.py
@@ -1,10 +1,12 @@
 from numpy import ndarray
 from enum import IntEnum
+from multiprocessing.connection import PipeConnection
 # > Typing
 from typing_extensions import (
+    Any,
     Literal,
     NamedTuple, TypedDict,
-    Generic, TypeVar, TypeAlias
+    Generic, TypeVar, TypeAlias, overload
 )
 # > Local Imports
 from seaplayer_audio._types import AudioSamplerate, AudioChannels, AudioDType
@@ -37,3 +39,33 @@ PacketTypes: TypeAlias = \
     Packet[Literal[PacketType.STOP], None] | \
     Packet[Literal[PacketType.INIT], InitData] | \
     Packet[Literal[PacketType.AUDIO], bytes]
+
+
+class StreamerAPI:
+    def __init__(self, connection: PipeConnection) -> None:
+        self.connection = connection
+    
+    def send(self, type: PacketType, data: Any, *, verify: bool=True) -> None:
+        self.connection.send(Packet(type, data))
+        if verify:
+            answer = self.connection.recv()
+            if answer.type == PacketType.ERROR:
+                raise answer.data
+    
+    def recv(self) -> PacketTypes:
+        return self.connection.recv()
+    
+    def s_error(self, data: Exception, /) -> None:
+        self.send(PacketType.ERROR, data, verify=False)
+    
+    def s_ok(self) -> None:
+        self.send(PacketType.OK, None, verify=False)
+    
+    def s_init(self, data: InitData, /) -> None:
+        self.send(PacketType.INIT, data, verify=True)
+    
+    def s_stop(self) -> None:
+        self.send(PacketType.STOP, None, verify=True)
+    
+    def s_audio(self, data: bytes, /) -> None:
+        self.send(PacketType.AUDIO, data, verify=True)
--- seaplayer_audio/streamers/mpstreamersnd/steamer.py
+++ seaplayer_audio/streamers/mpstreamersnd/steamer.py
@@ -6,14 +6,16 @@ from threading import Thread
 from multiprocessing import Process, Pipe
 # > Typing
 from typing_extensions import (
+    Any,
     Optional,
+    Callable,
     deprecated
 )
 # > Local Imports
 from seaplayer_audio.base import SoundDeviceStreamerBase, StreamerState
 from seaplayer_audio._types import AudioSamplerate, AudioChannels, AudioDType
 from seaplayer_audio.streamers.mpstreamersnd.process import __process__
-from seaplayer_audio.streamers.mpstreamersnd._types import Packet, PacketType, PacketTypes
+from seaplayer_audio.streamers.mpstreamersnd._types import Packet, PacketType, PacketTypes, StreamerAPI
 
 # ! Multiprocessing SoundDevice Streamer Class
 
@@ -25,43 +27,34 @@ class MPSoundDeviceStreamer(SoundDeviceStreamerBase):
         channels: Optional[AudioChannels]=None,
         dtype: Optional[AudioDType]=None,
         closefd: bool=True,
-        device: Optional[int]=None
+        device: Optional[int]=None,
+        *,
+        run_loop: Optional[Callable[[StreamerAPI], Any]]=None
     ) -> None:
         super().__init__(samplerate, channels, dtype, closefd, device)
+        self.run_loop = run_loop if (run_loop is not None) else self.run
         self.parent_pipe, self.child_pipe = Pipe()
-        self.queue = queue.Queue(1)
-        self.thread = Thread(target=self.run)
-        self.process = Process(name=f'<{self.__class__.__name__} from {id(self)}>', target=__process__, args=(self.child_pipe,))
-    
+        self.queue: queue.Queue[PacketTypes] = queue.Queue(1)
+        self.api = StreamerAPI(self.parent_pipe)
+        self.thread = Thread(target=self.run_loop, args=(self.api,))
+        self.process = Process(name=f'<{self.__class__.__name__} from {hex(id(self))}>', target=__process__, args=(self.child_pipe,))
+
     def is_busy(self) -> bool:
         return self.queue.qsize() >= self.queue.maxsize
     
-    def run(self):
-        self.parent_pipe.send(
-            Packet(
-                PacketType.INIT,
-                {'samplerate': self.samplerate, 'channels': self.channels, 'dtype': self.dtype, 'device': self.device}
-            )
-        )
-        packet: PacketTypes = self.parent_pipe.recv()
-        if packet.type == PacketType.ERROR:
-            raise packet.data
+    def run(self, api: StreamerAPI):
+        api.s_init({'samplerate': self.samplerate, 'channels': self.channels, 'dtype': self.dtype, 'device': self.device})
         self.state |= StreamerState.STARTED
         while StreamerState.RUNNING in self.state:
             try:
-                packet: PacketTypes = self.queue.get(timeout=3.0)
+                packet = self.queue.get(timeout=3.0)
                 if packet.type == PacketType.INIT:
-                    self.parent_pipe.send(packet)
-                    packet: PacketTypes = self.parent_pipe.recv()
-                    if packet.type == PacketType.ERROR:
-                        raise packet.data
+                    api.s_init(packet.data)
                 elif packet.type == PacketType.AUDIO:
-                    self.parent_pipe.send(packet)
-                    self.parent_pipe.recv()
+                    api.s_audio(packet.data)
             except queue.Empty:
                 pass
-        self.parent_pipe.send(Packet(PacketType.STOP, None))
-        self.parent_pipe.recv()
+        api.s_stop()
         self.state &= ~StreamerState.STARTED
     
     def start(self) -> None:
